d9e30a9358c55917b0944e100f302ddb36ddee0e
Wanderley Caloni <wanderley@caloni.com.br>
2012-06-16 10:44:06 -0300

    Ioclt Handl

--- IoctlCopy/Driver/Driver.cpp
+++ IoctlCopy/Driver/Driver.cpp
@@ -55,7 +55,62 @@ OnClose(PDEVICE_OBJECT  pDeviceObj,
     return STATUS_SUCCESS;
 }
 
+NTSTATUS IoctlCopyBuffered(PDEVICE_OBJECT pDeviceObj, PIRP pIrp) 
+{
+	KdPrint(("IoctlCopy.sys >> IoctlCopyBuffered\n"));
+	NTSTATUS nts = STATUS_NOT_SUPPORTED;
 
+	return nts;
+}
+
+NTSTATUS IoctlCopyDirect(PDEVICE_OBJECT pDeviceObj, PIRP pIrp) 
+{
+	KdPrint(("IoctlCopy.sys >> IoctlCopyDirect\n"));
+	NTSTATUS nts = STATUS_NOT_SUPPORTED;
+
+	return nts;
+}
+
+NTSTATUS IoctlCopyNeither(PDEVICE_OBJECT pDeviceObj, PIRP pIrp) 
+{
+	KdPrint(("IoctlCopy.sys >> IoctlCopyNeither\n"));
+	NTSTATUS nts = STATUS_NOT_SUPPORTED;
+
+	return nts;
+}
+
+NTSTATUS OnIoControl(PDEVICE_OBJECT pDeviceObj, 
+		PIRP pIrp)
+{
+    KdPrint(("IoctlCopy.sys >> OnIoControl\n"));
+    NTSTATUS nts = STATUS_NOT_SUPPORTED;
+    PIO_STACK_LOCATION pIoStackIrp = IoGetCurrentIrpStackLocation(pIrp);    
+
+    if(pIoStackIrp) /* Should Never Be NULL! */
+    {
+        switch(pIoStackIrp->Parameters.DeviceIoControl.IoControlCode)
+        {
+            case IOCTL_COPY_BUFFERED:
+                 nts = IoctlCopyBuffered(pDeviceObj, pIrp);
+                 break;
+
+            case IOCTL_COPY_DIRECT:
+                 nts = IoctlCopyDirect(pDeviceObj, pIrp);
+                 break;
+
+            case IOCTL_COPY_NEITHER:
+                 nts = IoctlCopyNeither(pDeviceObj, pIrp);
+                 break;
+        }
+	}
+	
+	pIrp->IoStatus.Status = nts;
+
+    IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+
+    return nts;
+
+}
 
 extern "C" NTSTATUS
 DriverEntry(IN PDRIVER_OBJECT   pDriverObj,
@@ -70,6 +125,7 @@ DriverEntry(IN PDRIVER_OBJECT   pDriverObj,
     pDriverObj->MajorFunction[IRP_MJ_CREATE] = OnCreate;
     pDriverObj->MajorFunction[IRP_MJ_CLEANUP] = OnCleanup;
     pDriverObj->MajorFunction[IRP_MJ_CLOSE] = OnClose;
+	pDriverObj->MajorFunction[IRP_MJ_DEVICE_CONTROL]    = OnIoControl;
 
     pDriverObj->DriverUnload = OnDriverUnload;
 
